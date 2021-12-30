187f1d6846902e74ba0d010e831182948a218e52
nvg25 <nvg25@bk.ru>
2021-12-30 18:34:13 +0500

    Добавьте файлы проекта.

---
+++ Task_17/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17
+{
+    class Account<T> //создаем обобщенный класс Account
+    {
+        //автоствойства
+        public string Name { get; set; }
+        public T AccountNumber { get; set; }
+        public int Balance { get; set; }
+
+        //конструктор
+        public Account(string name, T accountNumber, int balance)
+        {
+            Name = name;
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        
+        public void Print() //метод выводящий информацию на консоль
+        {
+            Console.WriteLine("Товарищ {0} \n Номер счета: {1} \n Баланс {2}", Name, AccountNumber, Balance);
+        }
+
+    }
+}
---
+++ Task_17/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ФИО:"); 
+            string name = Console.ReadLine(); //ввод имени с консоли
+            Console.WriteLine("Введите числовой номер счета:");
+            int accountNumber = Convert.ToInt32(Console.ReadLine());  //ввод номера с консоли
+            Console.WriteLine("Введите баланс:");
+            int balance = Convert.ToInt32(Console.ReadLine()); //ввод баланса с консоли
+
+            Account<int> account1 = new Account<int>(name, accountNumber, balance); //создаем экземпляр класса Account с числовым номером
+
+            account1.Print(); //вызов метода Print для экземпляра account1
+
+
+            Console.WriteLine("Введите ФИО:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите строковый номер счета:");
+            string accountNumberString = Console.ReadLine();
+            Console.WriteLine("Введите баланс:");
+            balance = Convert.ToInt32(Console.ReadLine());
+            
+            Account<string> account2 = new Account<string>(name, accountNumberString, balance);  //создаем экземпляр класса Account со строковым номером
+
+            account2.Print(); //вызов метода Print для экземпляра account2
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+    
+}
+
